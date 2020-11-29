c4ef554f8c7762974109ab9415c9b94b1c84c5ed
Max Hartke <hartke.max@gmail.com>
2020-11-28 19:29:11 -0600

    Added InvertedIndex class

---
+++ src/com/java/InvertedIndex.java
@@ -0,0 +1,13 @@
+package com.java;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class InvertedIndex {
+    private DictionaryInterface<String, LList<Integer>> wordDictionary;
+
+    public InvertedIndex() {
+    }
+    public void readFile(Scanner data){
+
+    }
+}
--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,9 +1,12 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        InvertedIndex invertedIndex = new InvertedIndex();
 
     }
 }
