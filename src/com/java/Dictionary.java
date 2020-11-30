2282d965f6f07ad7a09661f5de25f27253c69eda
Melancholy22 <56004966+Melancholy22@users.noreply.github.com>
2020-11-29 16:28:05 -0800

    Update Dictionary.java

--- src/com/java/Dictionary.java
+++ src/com/java/Dictionary.java
@@ -1,5 +1,62 @@
 package com.java;
 
-public class Dictionary {
+import java.util.Iterator;
+
+public class Dictionary implements DictionaryInterface
+{
+
+	@Override
+	public Object add(Object key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getValue(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator getKeyIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator getValueIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
