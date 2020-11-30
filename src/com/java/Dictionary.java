42e2f57b6d49766fb7eb96b168054965e9ddf020
aporria <aporria0451@gmail.com>
2020-11-29 17:35:56 -0800

    Update Dictionary.java
    added add method

--- src/com/java/Dictionary.java
+++ src/com/java/Dictionary.java
@@ -1,14 +1,56 @@
 package com.java;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Dictionary implements DictionaryInterface
+public class Dictionary<K extends Comparable<? super K>, V> 
+			implements DictionaryInterface<K, V>
 {
+	private Node firstNode;
+	private int  numberOfEntries;
 
-	@Override
-	public Object add(Object key, Object value) {
-		// TODO Auto-generated method stub
-		return null;
+	public Dictionary()
+	{
+		initializeDataFields();
+	}
+	
+	public V add(K key, V value) 
+	{
+		V result = null;
+		if ((key == null) || (value == null))
+			throw new IllegalArgumentException("Cannot add null to a dictionary.");
+		else
+		{
+			Node currentNode = firstNode;
+			Node nodeBefore = null;
+			while ((currentNode != null) && key.compareTo(currentNode.getKey()) > 0)
+			{
+				nodeBefore = currentNode;
+				currentNode = currentNode.getNextNode();
+			} // end while
+
+			if ( (currentNode != null) && key.equals(currentNode.getKey()) )
+			{
+				result = currentNode.getValue();
+				currentNode.setValue(value);
+			}
+			else
+			{
+				Node newNode = new Node(key, value);
+				if (nodeBefore == null)
+				{
+					newNode.setNextNode(firstNode); 
+					firstNode = newNode;
+				}
+				else
+				{
+					newNode.setNextNode(currentNode);
+					nodeBefore.setNextNode(newNode);
+				}
+				numberOfEntries++;
+			}
+		}
+		return result;		
 	}
 
 	@Override
