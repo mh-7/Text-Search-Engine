3654001b2eaf71fa2364b3345394c77f6ac67c20
ArdavanSh <ArdavanSh@users.noreply.github.com>
2020-11-29 16:45:20 -0800

    ReadFile method

--- src/com/java/InvertedIndex.java
+++ src/com/java/InvertedIndex.java
@@ -1,13 +1,42 @@
 package com.java;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class InvertedIndex {
     private DictionaryInterface<String, LList<Integer>> wordDictionary;
 
     public InvertedIndex() {
     }
-    public void readFile(Scanner data){
+    public void readFile(){
+    	
+    	String fileName;
+    	String str;
+    	
+    	for(int i=1;i<401;i++)
+    	{
+    		fileName="Text-" +i+".txt";
+    		
+    		try
+            {
+    			   File file = new File (fileName);
+    	           Scanner scan = new Scanner(file);
+    	           
+    	           while(scan.hasNext())
+    	           {
+    	        	   str=scan.next();
+    	        	   
+    	        	   //here we can call Tokenize method or anything else
+    	    			
+    	           }
+            }
+    		catch(IOException io)
+            {
+                System.out.println("The file not found");
+            }
+
+    	}
+    	
+    	
 
     }
 }
