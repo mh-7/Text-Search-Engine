5fe6a4a1f5f037f6c6c26cd8e196d7e73592eceb
Max Hartke <hartke.max@gmail.com>
2020-12-03 17:23:00 -0900

    Fixed merged conflict

--- src/com/java/InvertedIndex.java
+++ src/com/java/InvertedIndex.java
@@ -3,87 +3,78 @@ import java.io.*;
 import java.util.*;
 
 public class InvertedIndex {
-    public DictionaryInterface<String, ListInterface<Integer>> wordDictionary;
-    private static int idNum;
+	public DictionaryInterface<String, ListInterface<Integer>> wordDictionary;
+	private static int idNum;
 
-    public InvertedIndex() {
-    	wordDictionary = new Dictionary<>();
-    	
-    }
-    public void readFile(){
-    	String fileName;
-    	String str;
-    	
-    	
-    	for(int i=1;i<2;i++)
-    	{
-    		idNum=1;
-    		fileName="Text-" +i+".txt";
-    		
-    		try { //src/com/java/collection/Text-1.txt
-            {
-    			   File file = new File ("src\\com\\java\\collection\\"+fileName);
+	public InvertedIndex() {
+		wordDictionary = new Dictionary<>();
 
-    	           Scanner scan = new Scanner(file);
-    	           
-    	           while(scan.hasNext())
-    	           {
-    	        	   str=scan.next();
-    	        	   //here we can call Tokenize method or anything else
-    	        	   str = tokenize(str);
-    	       
-    	        	   if(stopList(str) && !wordDictionary.contains(str))
-    	        	   {
-  
-    	        		   ListInterface<Integer> idList=new LList<>();
-    	        		   wordDictionary.add(str, idList);
-    	        		   
-    	        	   }
-    	   
-    	    			
-    	           }
-            }
+	}
+
+	public void readFile() {
+		String fileName;
+		String str;
+
+
+		for (int i = 1; i < 2; i++) {
+			idNum = 1;
+			fileName = "Text-" + i + ".txt";
+
+			try {
+				{
+					File file = new File("src\\com\\java\\collection\\" + fileName);
+
+					Scanner scan = new Scanner(file);
+
+					while (scan.hasNext()) {
+						str = scan.next();
+						//here we can call Tokenize method or anything else
+						str = tokenize(str);
+
+						if (stopList(str) && !wordDictionary.contains(str)) {
+
+							ListInterface<Integer> idList = new LList<>();
+							wordDictionary.add(str, idList);
+
+						}
+
+
+					}
+				}
+			}
     		catch(IOException io)
-            {
-                System.out.println("The file not found");
-            }
-    	}
+				{
+					System.out.println("The file not found");
+				}
+		}
 
-//    	}
-    	
-    	
+			}
+			//Can take in a word and return it without all the Punctuation and makes
+			//it all into lower case.
+			private String tokenize (String str)
+			{
+				String str1 = "";
+				String withOutPunc = str.replaceAll("[^a-zA-Z ]", "");
+				withOutPunc = withOutPunc.toLowerCase();
+				return withOutPunc;
+			}
 
-    }
-    //Can take in a word and return it without all the Punctuation and makes
-    //it all into lower case.
-    private String tokenize(String str)
-    {
-    	String str1 = "";
-    	String withOutPunc = str.replaceAll("[^a-zA-Z ]", "");
-    	withOutPunc = withOutPunc.toLowerCase();
-    	return withOutPunc;
-    }
-    
-    private boolean stopList(String str)
-    {
-    	boolean boo = true;
-    	String str1;
-    	
-    	try
-        {
-			   File file = new File ("src\\com\\java\\stop-list.txt");
-	           Scanner scan = new Scanner(file);
-	           while(scan.hasNext())
-	           {
-	        	   str1 = scan.next();
-	        	   if(str.equals(str1))
-	        		   boo = false;
-	           }
-        }
-		catch(IOException io)
-        {
-            System.out.println("The file not found");
-        }
-    	return boo;
-    }
+			private boolean stopList (String str)
+			{
+				boolean boo = true;
+				String str1;
+
+				try {
+					File file = new File("src\\com\\java\\stop-list.txt");
+					Scanner scan = new Scanner(file);
+					while (scan.hasNext()) {
+						str1 = scan.next();
+						if (str.equals(str1))
+							boo = false;
+					}
+				} catch (IOException io) {
+					System.out.println("The file not found");
+				}
+				return boo;
+			}
 }
