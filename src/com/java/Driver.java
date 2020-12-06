c27f64ded0b5aeeeb846b3216419eedbf28e2d23
Max Hartke <hartke.max@gmail.com>
2020-12-06 11:07:09 -0900

    added loop to driver

--- src/com/java/Driver.java
+++ src/com/java/Driver.java
@@ -9,7 +9,16 @@ public class Driver {
 		// TODO Auto-generated method stub
 		InvertedIndex ii=new InvertedIndex();
 		ii.readFile();
-		ii.documentMatch();
+		Boolean c = true;
+		while(c) {
+			ii.documentMatch();
+			System.out.println("Type s to search again and q to quit");
+			Scanner scanner = new Scanner(System.in);
+			String str = scanner.next();
+			if(str.equals("q")) {
+				c = false;
+			}
+		}
 	}
 
 }
